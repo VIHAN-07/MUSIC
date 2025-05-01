d8382db620a95b01b8309ee5d69ff0005418accd
advaitvarade <159159199+advaitvarade@users.noreply.github.com>
2025-05-01 16:13:19 +0530

    Update Buzzer.c

--- New folder/Buzzer.c
+++ New folder/Buzzer.c
@@ -1,40 +1,30 @@
-#include <stdio.h>
-#include <stdlib.h>
 #include<xc.h>
-
 #define _XTAL_FREQ 48000000
-volatile int count=0;
-
-void inittimer1() {
-    T1CON = 0b00110000;     // Prescaler 1:8, Timer1 OFF
-    TMR1H = 0x00;
-    TMR1L = 0x00;
-    INTCONbits.GIE=1;
-    INTCONbits.PEIE=1;
-    PIR1bits.TMR1IF = 0;    // Clear flag
-    T1CONbits.TMR1ON = 1;   // Start timer
-    PIE1bits.TMR1IE=1;
+void inittimer()
+{
+    T1CON=0b00110000;
+    TMR1L=0x00;
+    TMR1H=0x00;
+    TMR1IF=0;
+    TMR1IE=1;
+    T1CONbits.TMR1ON=1;
 }
-
-
-void __interrupt() ISR(){
-    if(PIR1bits.TMR1IF){
-        PIR1bits.TMR1IF = 0;
-        TMR1H = 0x00;
-        TMR1L = 0x00;
-        count++;
-        if(count==100){
-            LATAbits.LATA5 = ~LATAbits.LATA5;  
-            count=0;
-        }    
-    }    
+void main(void){
+    inittimer();
+    TRISAbits.TRISA5=0;
+    LATAbits.LATA5=1;
+    int count=0;
+    while(1){
+        if(TMR1IF){
+            TMR1IF=0;
+            TMR1L=0x00;
+            TMR1H=0x00;
+            count++;
+            if (count==50){  
+                LATAbits.LATA5=~ LATAbits.LATA5;
+                count=0;
+            }
+        }
+    }
+    return;
 }
-
-int main() {
-    TRISAbits.TRISA5 = 0;    // Output pin
-    LATAbits.LATA5 = 1;
-    inittimer1();
-    while (1);
-      
-}
-
